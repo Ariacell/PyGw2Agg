108676d0710ca54a0cce92743e74304b352613ee
Aria <aria.jackson@protonmail.com>
2023-10-21 18:20:18 +1100

    Improve feedback when clicking to parse logs

--- pygw2agg/ui/parsing.py
+++ pygw2agg/ui/parsing.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-from PySimpleGUI import Button, popup_error_with_traceback, Window
+import time
+from PySimpleGUI import Button, popup_error_with_traceback, Window, Text
 import structlog
 
 from pygw2agg.ui.settings import get_settings_path, get_user_settings
@@ -23,8 +24,27 @@ def get_logs_from_directory(input_directory):
     return res
 
 
+PARSING_IN_PROGRESS_KEY = "-PARSING_IN_PROGRESS_TEXT-"
+AGGREGATING_IN_PROGRESS_KEY = "-AGGREGATING_IN_PROGRESS_TEXT-"
+
+
+def get_progress_info_button(current_operation: str, key):
+    return Text(current_operation, key=key, visible=True)
+
+
 def handle_parse_event(window: Window, event, values):
     try:
+        window.extend_layout(
+            window,
+            [
+                [
+                    get_progress_info_button(
+                        "Parsing .zevtc logs...", PARSING_IN_PROGRESS_KEY
+                    )
+                ]
+            ],
+        )
+        window.refresh()
         settings = get_user_settings(get_settings_path())
         logger.debug(f"Using settings: {settings}")
 
@@ -59,14 +79,35 @@ def handle_parse_event(window: Window, event, values):
     except Exception as e:
         popup_error_with_traceback(f"Error while attempting to parse logs: {e}")
 
-    headings = ["Name", "Class Mark", "Age", "Homeroom Class"]
-    data = [
-        ["Jason", 31, 15, "A"],
-        ["John", 92, 16, "B"],
-        ["Ann", 77, 17, "C"],
-        ["Charlie", 18, 17, "D"],
-        ["Sarah", 55, 14, "A"],
-    ]
-    window.extend_layout(window["-MAIN_COL-"], [get_table(data, headings)])
+    window[PARSING_IN_PROGRESS_KEY].update(visible=False)
+    window[PARSING_IN_PROGRESS_KEY].Widget.master.pack_forget()
+    try:
+        window.extend_layout(
+            window,
+            [
+                [
+                    get_progress_info_button(
+                        "Aggregating JSON logs...", AGGREGATING_IN_PROGRESS_KEY
+                    )
+                ]
+            ],
+        )
+        window.refresh()
+        time.sleep(5)
+        headings = ["Name", "Class Mark", "Age", "Homeroom Class"]
+        data = [
+            ["Jason", 31, 15, "A"],
+            ["John", 92, 16, "B"],
+            ["Ann", 77, 17, "C"],
+            ["Charlie", 18, 17, "D"],
+            ["Sarah", 55, 14, "A"],
+        ]
+
+        window[AGGREGATING_IN_PROGRESS_KEY].update(visible=False)
+        window[AGGREGATING_IN_PROGRESS_KEY].Widget.master.pack_forget()
+        window.extend_layout(window["-MAIN_COL-"], [get_table(data, headings)])
+    except Exception as e:
+        popup_error_with_traceback(f"Error while attempting to parse logs: {e}")
+
     # C:/Users/Aria/Desktop/Gw2/dev/GW2EI_Release/GuildWars2EliteInsights.exe
     # C:\Users\Aria\Desktop\Gw2\Logs\2023_05_14_DadLossLATE_Thief_15s
